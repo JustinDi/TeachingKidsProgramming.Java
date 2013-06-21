2c5fbe1aaede537abb9136be2e48dd63323782a7
Justin Diaz <marthaleticiad@hotmail.com>
2013-06-20 21:01:36 -0700

    pentagon crazy prog2 - justin

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/PentagonCrazy.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/PentagonCrazy.java
@@ -1,35 +1,41 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.logo.ColorWheel;
+import org.teachingextensions.logo.Colors.Blues;
+import org.teachingextensions.logo.Colors.Purples;
 import org.teachingextensions.logo.Tortoise;
 
 public class PentagonCrazy
 {
   public static void main(String[] args)
   {
-    //    Make the tortoise move as fast as possible --#3
-    //    createColorPalette (recipe below) --#8
+    Tortoise.setSpeed(10);
     //    ------------- Recipe for createColorPalette --#8
-    //    Add steel blue to the color wheel --#7
-    //    Add dark orchid to the color wheel --#11
-    //    Add dark slate blue to the color wheel --#12
-    //    Add teal to the color wheel --#13
-    //    Add indigo to the color wheel --#14
+    createColorPalette();
     //    ------------- End of createColorPalette recipe 
     //    DrawPentagon (recipe below) --#10
     //    ------------- Recipe for drawPentagon --#10
     //    Do the following 200 times --#2
     //        AdjustPen (recipe below) --#9
     //        ------------- Recipe for adjustPen --#9
-    //        Change the color of the line the tortoise draws to the next color on the color wheel --#6
-    //        Increase the tortoises pen width by 1 --#15                                              
+    Tortoise.setPenColor(ColorWheel.getNextColor());
+    //        Increase the tortoises pen width by 1 --#15          
+    Tortoise.setPenWidth(+1);
     //        If the tortoises pen width is greater than 4, then --#17
     //            Reset it to 1 --#16
     //        ------------- End of adjustPen recipe 
     //        Move the tortoise the length of a side --#4
-    //        Turn the tortoise 1/5th of 360 degrees --#1
     Tortoise.turn(360 / 5);
     //        Turn the tortoise 1 more degree --#5
     //    Repeat
     //    ------------- End of drawPentagon recipe
+  }
+  private static void createColorPalette()
+  {
+    ColorWheel.addColor(Blues.SteelBlue);
+    ColorWheel.addColor(Purples.DarkOrchid);
+    ColorWheel.addColor(Blues.DarkSlateBlue);
+    ColorWheel.addColor(Blues.Teal);
+    ColorWheel.addColor(Purples.Indigo);
   }
 }
