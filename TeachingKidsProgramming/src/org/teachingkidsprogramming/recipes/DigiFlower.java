916f90452f14edd32ab6295e41a0d57e054be008
Justin Diaz <marthaleticiad@hotmail.com>
2013-06-20 18:58:17 -0700

    digiflower progress 1- Justin

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/DigiFlower.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/DigiFlower.java
@@ -1,10 +1,14 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.logo.ColorWheel;
+import org.teachingextensions.logo.Colors.Reds;
+import org.teachingextensions.logo.Tortoise;
+
 public class DigiFlower
 {
   public static void main(String[] args)
   {
-    //    Show the tortoise --#1
+    Tortoise.show();
     //    Make the tortoise move as fast as possible --#7
     //    Make the background silver --#8
     //    Make the line the tortoise draws 3 pixels wide --#20
@@ -14,11 +18,11 @@ public class DigiFlower
     //      Turn the tortoise 1/15th of 360 degrees to the right --#18
     //    Repeat
     //    ------------- Recipe for createColorPalette --#9
-    //    Color 1 is red --#3
+    ColorWheel.addColor(Reds.Red);
     //    Color 2 is dark orange --#11
     //    Color 3 is gold --#12
     //    Color 4 is yellow --#13
-    //    Add color 1 to the color wheel --#3.1
+    ColorWheel.addColor(Reds.Red);
     //    Add color 2 to the color wheel --#11.1
     //    Add color 3 to the color wheel --#12.1
     //    Add color 4 to the color wheel --#13.1
@@ -29,7 +33,8 @@ public class DigiFlower
     //    ------------- Recipe for drawOctogon --#10
     //    Do the following 8 times --#6
     //      Change the color of the line the tortoise draws to the next color on the color wheel --#4
-    //      Move the tortoise 50 pixels --#2
+    Tortoise.setPenColor(ColorWheel.getNextColor());
+    Tortoise.move(50);
     //      Turn the tortoise 1/8th of 360 degrees to the right --#5
     //    Repeat  
   }
